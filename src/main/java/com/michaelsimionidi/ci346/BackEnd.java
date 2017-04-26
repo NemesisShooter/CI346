39503403ab5e552e11a96a46854fac279e1d0782
NemesisShooter <grekdarkness@gmail.com>
2017-04-26 15:32:18 +0100

    Setup

---
+++ src/main/java/com/michaelsimionidi/ci346/BackEnd.java
@@ -0,0 +1,14 @@
+package com.michaelsimionidi.ci346;
+
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class BackEnd {
+	
+	@RequestMapping("/api")
+	public String Index() {
+		return "{}";
+	}
+	
+}
