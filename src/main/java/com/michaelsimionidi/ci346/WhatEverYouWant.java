a49bcb9a5028ff035d3974a11b798b5a6e1ca48c
NemesisShooter <grekdarkness@gmail.com>
2017-04-28 13:54:02 +0100

    Update 1, WhateEverYouWant added, Spring boot use

---
+++ src/main/java/com/michaelsimionidi/ci346/WhatEverYouWant.java
@@ -0,0 +1,13 @@
+package com.michaelsimionidi.ci346;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class WhatEverYouWant {
+	
+	public static void main(String[] args)
+	{
+		SpringApplication.run(WhatEverYouWant.class, args);
+	}
+}
