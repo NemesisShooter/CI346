737064a9ba5966e95f57f2c6c16fcfd6ee41a03c
NemesisShooter <grekdarkness@gmail.com>
2017-05-04 15:29:12 +0100

    revert to update before auth.

--- src/main/java/com/michaelsimionidi/ci346/WhatEverYouWant.java
+++ src/main/java/com/michaelsimionidi/ci346/WhatEverYouWant.java
@@ -1,16 +1,11 @@
 package com.michaelsimionidi.ci346;
 
 import org.springframework.boot.SpringApplication;
-import org.springframework.boot.web.support.SpringBootServletInitializer;
-import org.springframework.context.annotation.ComponentScan;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
-@ComponentScan(basePackages = "com.michaelsimionidi")
 @SpringBootApplication
-public class WhatEverYouWant extends SpringBootServletInitializer {
-	
-	private static Class applicationClass = WhatEverYouWant.class;
-	
+public class WhatEverYouWant {
+
 	public static void main(String[] args)
 	{
 		SpringApplication.run(WhatEverYouWant.class, args);
